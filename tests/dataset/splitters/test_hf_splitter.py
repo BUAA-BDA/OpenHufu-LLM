30df472d1f9679f830d6c2e30abdbd477a5e8b2b
Dludora <koushurui@outlook.com>
2025-09-24 08:42:23 +0000

    format: splitter

--- tests/dataset/splitters/test_hf_splitter.py
+++ tests/dataset/splitters/test_hf_splitter.py
@@ -8,8 +8,8 @@ class TestHFIIDSplitter(unittest.TestCase):
 
     def setUp(self):
         data = {
-            'text': ['sample1', 'sample2', 'sample3', 'sample4', 'sample5', 'sample6'],
-            'label': [0, 1, 0, 1, 0, 1]
+            "text": ["sample1", "sample2", "sample3", "sample4", "sample5", "sample6"],
+            "label": [0, 1, 0, 1, 0, 1],
         }
         self.dataset = Dataset.from_dict(data)
         self.n_clients = 3
@@ -24,18 +24,22 @@ class TestHFLDASplitter(unittest.TestCase):
 
     def setUp(self):
         file_path = download_url(
-            'https://raw.githubusercontent.com/sahil280114/codealpaca/refs/heads/master/data/rosetta_alpaca.json',
-            '../../../dataset/')
-        self.dataset = load_dataset(get_file_path_without_name(file_path))['train']
+            "https://raw.githubusercontent.com/sahil280114/codealpaca/refs/heads/master/data/rosetta_alpaca.json",
+            "../../../dataset/",
+        )
+        self.dataset = load_dataset(get_file_path_without_name(file_path))["train"]
         self.n_clients = 3
         self.splitter = HFLDASplitter(n_clients=self.n_clients, seed=42)
 
     def test_split(self):
-        splits = self.splitter(self.dataset, get_labels_fn=lambda x: x['input'], alpha=0.8)
+        splits = self.splitter(
+            self.dataset, get_labels_fn=lambda x: x["input"], alpha=0.8
+        )
         import numpy as np
-        s = np.unique(splits[0]['input'])
+
+        s = np.unique(splits[0]["input"])
         self.assertEqual(len(splits), self.n_clients)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/dataset/splitters/test_list_splitter.py
+++ tests/dataset/splitters/test_list_splitter.py
@@ -1,15 +1,16 @@
 import unittest
 from openhufu.dataset.splitters.custom.list_splitter import ListIIDSplitter
 
+
 class TestListSplitter(unittest.TestCase):
     def setUp(self):
         self.dataset = [
-            {'text': 'sample1', 'label': 0},
-            {'text': 'sample2', 'label': 1},
-            {'text': 'sample3', 'label': 0},
-            {'text': 'sample4', 'label': 1},
-            {'text': 'sample5', 'label': 0},
-            {'text': 'sample6', 'label': 1}
+            {"text": "sample1", "label": 0},
+            {"text": "sample2", "label": 1},
+            {"text": "sample3", "label": 0},
+            {"text": "sample4", "label": 1},
+            {"text": "sample5", "label": 0},
+            {"text": "sample6", "label": 1},
         ]
         self.n_clients = 3
         self.splitter = ListIIDSplitter(n_clients=self.n_clients, seed=42)
@@ -18,5 +19,6 @@ class TestListSplitter(unittest.TestCase):
         splits = self.splitter(self.dataset)
         self.assertEqual(len(splits), self.n_clients)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
